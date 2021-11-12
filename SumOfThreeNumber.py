471b6a72b48472622f780cc4dae4b9a4d6162dd7
nitish7890 <82252897+nitish7890@users.noreply.github.com>
2021-11-12 10:09:33 +0530

    Delete SumOfThreeNumber.py

--- SumOfThreeNumber.py
+++
@@ -1,8 +0,0 @@
-def add( a, b,c):  #function definition
-    return(a+b+c);
-
-#dervied class    
-a=int(input("Enter the value of a"));
-b=int(input("Enter the value of b"));
-c=int(input("Enter the value of c"));
-print("SUM OF THREE NUMBER=>",add(a,b,c));  #calling function  
